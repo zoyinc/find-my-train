a5eada1bc610a62bff200bbba5fc2962973f258a
Tony Paterson <tony@cantabrian.co.nz>
2026-01-03 11:56:43 +1300

    Station initial setup prior to scripting changes

---
+++ wordpress/database/fmt_stations.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `fmt_stations` (
+  `section_id` int(11) DEFAULT NULL,
+  `section_name` varchar(100) DEFAULT NULL,
+  `featured` varchar(100) DEFAULT '"N"',
+  `primary_image` varchar(100) DEFAULT NULL,
+  `secondary_image` varchar(100) DEFAULT NULL,
+  `description` varchar(3000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
